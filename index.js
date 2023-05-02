7349e85d08c13339163b815ff7f9d4802894a41d
Patryk <sucheckipatryk@gmail.com>
2023-05-02 17:04:24 -0400

    9 passing tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(value = "roller-skate") {
+    return `This Saturday, I want to ${value}!`;
+};
+
+function mondayWork(value = "go to the office") {
+    return `This Monday, I will ${value}.`;
+};
+
+function wrapAdjective(highlight) {
+    return function(adjective) {
+        return `You are ${highlight}${adjective}${highlight}!`
+    }
+};
